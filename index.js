eed990c71c8338e24f481eaba19641727e4b96f6
DanSam%k <dansam5000@gmail.com>
2020-09-19 12:04:07 +0100

    Getting greens with proper code refactoring

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const Calculate = {
-
+    //
+    factorial(inputNum) {
+        for (let i = inputNum - 1; i >= 1; i--){
+            inputNum *= i;
+        }
+        return inputNum;
+    }
 }
 
 module.exports = Calculate;
--- test/index_test.js
+++ test/index_test.js
@@ -4,9 +4,25 @@ const Calculate = require("../index");
 describe("Calculate", () => {
     describe(".factorial", () => {
       it("will test if the output of 5! equal to 120", () => {
+        //setup
+        const expected = 120;
+        const inputNum = 5;
+        //exercise
+        const result = Calculate.factorial(inputNum);    
+        //verification
+        assert.equal(result, expected);
 
-        assert.equal(Calculate.factorial(5), 120);
+      })
+      
+      it("accept different input and output expected result", () => {
+        //setup
+        const expected = 24;
+        const inputNum = 4;
+        //exercise
+        const result = Calculate.factorial(inputNum);
+        //verification
+        assert.equal(result, expected)
 
-      })  
+      })
     })
 })
